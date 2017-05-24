f9f9913f7a3c20db71bcb6913efa449acd5937cd
d-graf <dario.graf@comvation.com>
2017-05-24 08:51:18 +0200

    Added loading screen + check connection

--- app/src/main/java/ch/sario/mapeminder/LoadingActivity.java
+++ app/src/main/java/ch/sario/mapeminder/LoadingActivity.java
@@ -1,13 +1,61 @@
 package ch.sario.mapeminder;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.StrictMode;
+import android.provider.Settings;
+import android.support.design.widget.BaseTransientBottomBar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.HttpURLConnection;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.xml.datatype.Duration;
+
 public class LoadingActivity extends AppCompatActivity {
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        if (!isNetworkAvailable()){
+            mDialog = ProgressDialog.show(this, "Keine Internetverbindung / GPS", "Bitte warten...");
+        }else{
+            procceedToNextActivity();
+        }
+
+    }
+
+    public void procceedToNextActivity()
+    {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
